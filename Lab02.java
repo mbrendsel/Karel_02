f97090e35b2897d2ce41ce04b0477b8c9a3dc8a4
mbrendsel <monica_brendsel@asl.org>
2014-11-21 10:27:45 +0000

    Update Lab02.java

--- Lab02.java
+++ Lab02.java
@@ -2,16 +2,61 @@
 *
 * An athlete navigates a maze while leaving a trail of beepers
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Monica  
+* @version 11/21/14
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
-public class Lab02 {
+ public class Lab02 extends Robot 
+{
+public static void main(String[] args) {
 
-     public static void main(String[] args) {
-          // TODO Your code goes here
+Display.openWorld("maps/maze.map");
+Display.setSize(8, 8);
+
+Athlete karel = new Athlete();
+ 
+   karel.putBeeper();
+   karel.move();
+   karel.putBeeper();
+   karel.turnRight();
+   karel.move();
+   karel.putBeeper();
+   karel.turnRight();
+   karel.move();
+   karel.putBeeper();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.turnLeft();
+   karel.putBeeper();
+   karel.turnRight();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.putBeeper();
+   karel.turnRight();
+   karel.move();
+   karel.putBeeper();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.turnLeft();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+   karel.putBeeper();
+   karel.turnRight();
+   karel.move();
+   karel.putBeeper();
+   karel.move();
+
      }
 }
