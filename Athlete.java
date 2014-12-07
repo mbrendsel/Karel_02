57544ed8dc2c6e586b9976a9e669f4c33aa58072
mbrendsel <monica_brendsel@asl.org>
2014-12-07 23:23:29 +0000

    Update Athlete.java

--- Athlete.java
+++ Athlete.java
@@ -13,20 +13,27 @@ import edu.fcps.karel2.Display;
 
 public class Athlete extends Robot {
 	
-	public Athlete() {
-		super(1,1,Display.NORTH, Display.INFINITY);
-	}
-	
-	public Athlete(int x, int y, int dir, int beep) {
-		super(x, y, dir, beep);
-	}
-	
-	public void turnAround() {
-		// TODO Your code goes here
-	}
-	
-	public void turnRight() {
-		// TODO Your code goes here
-	}
-
+	 public class Athlete extends Robot 
+{
+   public Athlete()
+  {
+     super(1, 1, Display.NORTH, Display.INFINITY);
+  }
+   public Athlete(int x, int y, int dir, int beep)
+  {
+     super(x, y, dir, beep);
+  }
+   public void turnAround()
+  {
+   turnLeft();
+   turnLeft();
+  }
+   public void turnRight()
+  {
+   turnLeft();
+   turnLeft();
+   turnLeft();
+  
+   }
 }
+  
